4d280a35098b00efde9c7074ad25485b80be0ed1
mathancode99 <rmathanmathan2306@gmail.com>
2025-04-30 12:32:54 +0530

    Add build.sh and render.yaml

--- Demo/build.sh
+++ Demo/build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-# install requirements
 pip install -r requirements.txt
-
-# run migrations
+python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-
-# (optional) load fixtures or create superuser (skip if not needed)
--- Demo/settings.py
+++ Demo/settings.py
@@ -143,6 +143,9 @@ STATICFILES_DIRS =[STATIC_DIR]
 MEDIA_URL = '/media/'                             
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')       
 STATIC_ROOT=os.path.join(BASE_DIR,'static/media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
